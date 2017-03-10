549a7f27293d2edefa09a6f37b27ad0f77cfa7f6
Ian parcs <ianparcs@gmail.com>
2017-03-10 11:38:53 +0800

    add item

--- desktop/src/edu/uslt/cs/thesis/gis/desktop/DesktopLauncher.java
+++ desktop/src/edu/uslt/cs/thesis/gis/desktop/DesktopLauncher.java
@@ -2,12 +2,16 @@ package edu.uslt.cs.thesis.gis.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import edu.uslt.cs.thesis.gis.core.GIS;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Geographical Information System: University of Saint Louis Campus";
-		new LwjglApplication(new GIS(), config);
-	}
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Geographical Information System: University of Saint Louis Campus";
+        config.width = 673;
+        config.height = 616;
+        new LwjglApplication(new GIS(), config);
+    }
 }
